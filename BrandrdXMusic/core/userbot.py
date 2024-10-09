f3d906075eb75d94982c2e3fdf46efc5987157c9
NewTest56 <ayushd78900@gmail.com>
2024-10-09 13:13:35 +0530

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -71,8 +71,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
+                await self.one.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
+                await self.one.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
                 await self.one.join_chat("BRANDRD_BOT")
                 await self.one.join_chat("ABOUT_BRANDEDKING")
 
@@ -101,8 +101,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.two.join_chat("BRANDED_PAID_CC")
+                await self.two.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
+                await self.two.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
                 await self.two.join_chat("BRANDRD_BOT")
                 await self.two.join_chat("ABOUT_BRANDEDKING")
             except:
@@ -125,8 +125,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.three.join_chat("BRANDED_PAID_CC")
+                await self.three.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
+                await self.three.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
                 await self.three.join_chat("BRANDRD_BOT")
                 await self.three.join_chat("ABOUT_BRANDEDKING")
             except:
@@ -148,8 +148,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.four.join_chat("BRANDED_PAID_CC")
+                await self.four.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
+                await self.four.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
                 await self.four.join_chat("BRANDRD_BOT")
                 await self.four.join_chat("ABOUT_BRANDEDKING")
             except:
@@ -171,8 +171,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.five.join_chat("BRANDED_PAID_CC")
+                await self.five.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
+                await self.five.join_chat("https://t.me/+_qPchBV5z4VjMjM1")
                 await self.five.join_chat("BRANDRD_BOT")
                 await self.five.join_chat("ABOUT_BRANDEDKING")
             except:
